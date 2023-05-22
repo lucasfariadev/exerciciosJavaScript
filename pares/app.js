640d6cbbaff2a8b848cc61e0a0c0b992d0889258
Lucas Faria <74054553+lucasfariadev@users.noreply.github.com>
2023-05-21 22:29:18 -0300

    Update app.js

--- pares/app.js
+++ pares/app.js
@@ -1,12 +1,19 @@
 function contaPares(){
     var valor = parseInt(document.getElementById("valor").value);
     var divResultado = document.getElementById("resultado");
+    var cont = 0;
+    divResultado.innerHTML = "";
     for(i=0; i <= valor; i++){
         if(i%2 == 0){
             console.log(i);
+            cont++;
             divResultado.innerHTML += ` ${i} `;
         }
-
+    
     }
+    if(cont == 0){
+        divResultado.innerHTML = "não há números pares";
+    }
+    cont = 0;
 
 }
